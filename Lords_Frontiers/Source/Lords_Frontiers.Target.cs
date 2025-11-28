c0c1b7099cc970729939e5769eb6544743736b1d
TToJlkoBHuK <28arhalex@gmail.com>
2025-11-28 20:59:33 +0300

    realization block ResourceManager #41 #13

---
+++ Lords_Frontiers/Source/Lords_Frontiers.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Lords_FrontiersTarget : TargetRules
+{
+	public Lords_FrontiersTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+
+		ExtraModuleNames.AddRange( new string[] { "Lords_Frontiers" } );
+	}
+}
