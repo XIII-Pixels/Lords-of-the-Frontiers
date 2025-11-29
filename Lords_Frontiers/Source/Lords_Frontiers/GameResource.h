9e7cc6d05fa018a024a9d836e6a6350d32c9ffa2
TToJlkoBHuK <28arhalex@gmail.com>
2025-11-29 21:01:36 +0300

    adjusting the enum #29
    enum has been redesigned to meet the standard

--- Lords_Frontiers/Source/Lords_Frontiers/GameResource.h
+++ Lords_Frontiers/Source/Lords_Frontiers/GameResource.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "ResourceTypes.generated.h"
+#include "GameResource.generated.h"
 
-//Enum types of resources available in the game
+// Enum types of resources available in the game
 UENUM(BlueprintType)
 enum class EResourceType : uint8
 {
@@ -13,9 +13,9 @@ enum class EResourceType : uint8
 	Mana UMETA(DisplayName = "Mana")
 };
 
-//A structure representing the storage unit of a resource (type + quantity)
+// A structure representing the storage unit of a resource (type + quantity)
 USTRUCT(BlueprintType)
-struct FResource
+struct FGameResource
 {
 	GENERATED_BODY()
 
@@ -26,23 +26,23 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource", meta = (ClampMin = "0"))
 	int32 Quantity;
 
-	//Default c-tor
-	FResource()
+	// Default c-tor
+	FGameResource()
 		: Type(EResourceType::None)
 		, Quantity(0)
 	{
 	}
 
-	//C-tor with params
-	FResource(EResourceType InType, int32 InQuantity)
-		: Type(InType)
-		, Quantity(InQuantity)
+	// C-tor with params. Params in lowerCamelCase
+	FGameResource(EResourceType type, int32 quantity)
+		: Type(type)
+		, Quantity(quantity)
 	{
 	}
 
-	//Comparison operator for convenience of checks
-	bool operator==(const FResource& Other) const
+	// Comparison operator. Param 'other' in lowerCamelCase
+	bool operator==(const FGameResource& other) const
 	{
-		return Type == Other.Type && Quantity == Other.Quantity;
+		return Type == other.Type && Quantity == other.Quantity;
 	}
 };
