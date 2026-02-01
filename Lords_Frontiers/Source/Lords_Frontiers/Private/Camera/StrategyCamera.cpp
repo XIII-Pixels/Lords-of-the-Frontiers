edc8d614d0872f726693aa1828ac679238f0f88c
TToJlkoBHuK <28arhalex@gmail.com>
2026-02-01 22:03:37 +0300

    realization game camera #211
    1) Создан класс AStrategyCamera на базе APawn для эффективного управления камерой в RTS-стиле
    2) Реализовано плавное перемещение по осям X и Y (WASD)
    3) Добавлен механизм зума (колесико мыши)
    4) Реализовано ограничение перемещения камеры по границам карты и лимиты приближения/отдаления
    5) Настроен фиксированный изометрический ракурс
    6) Добавлен киллер в BeginPlay для автоматического удаления конфликтующих CameraActor на сцене
    7) Логика перемещения нормализована по горизонтальной плоскости

---
+++ Lords_Frontiers/Source/Lords_Frontiers/Private/Camera/StrategyCamera.cpp
@@ -0,0 +1,128 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Camera/StrategyCamera.h"
+
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "Components/InputComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Camera/CameraActor.h"
+
+#include "GameFramework/PawnMovementComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+
+// Sets default values
+AStrategyCamera::AStrategyCamera()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>( TEXT( "Root" ) );
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>( TEXT( "SpringArm" ) );
+	SpringArm->SetupAttachment( RootComponent );
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bEnableCameraLag = true;
+
+	SpringArm->bUsePawnControlRotation = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritRoll = false;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>( TEXT( "Camera" ) );
+	Camera->SetupAttachment( SpringArm );
+	Camera->bUsePawnControlRotation = false;
+
+	MovementComponent = CreateDefaultSubobject<UFloatingPawnMovement>( TEXT( "MovementComponent" ) );
+
+	TargetZoom = 1500.0f;
+
+}
+
+// Called when the game starts or when spawned
+void AStrategyCamera::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FVector StartingLocation = GetActorLocation();
+	StartingLocation.Z = 0.0f;
+	SetActorLocation( StartingLocation );
+
+	MovementComponent->MaxSpeed = MoveSpeed;
+	MovementComponent->Acceleration = MoveAcceleration;
+	MovementComponent->Deceleration = MoveDeceleration;
+
+	SpringArm->TargetArmLength = TargetZoom;
+	SpringArm->CameraLagSpeed = CameraLagSpeed;
+	SpringArm->SetRelativeRotation( FRotator( CameraPitch, CameraYaw, 0.0f ) );
+
+	TArray<AActor*> FoundCameras;
+	UGameplayStatics::GetAllActorsOfClass( GetWorld(), ACameraActor::StaticClass(), FoundCameras );
+	for ( AActor* Cam : FoundCameras )
+	{
+		Cam->Destroy();
+	}
+
+	FTimerHandle TimerHandle;
+	GetWorldTimerManager().SetTimer(
+	    TimerHandle,
+	    [this]()
+	    {
+		    if ( APlayerController* pc = Cast<APlayerController>( GetController() ) )
+		    {
+			    pc->SetViewTarget( this );
+			    pc->bShowMouseCursor = true;
+			    pc->SetInputMode( FInputModeGameAndUI() );
+		    }
+	    },
+	    0.2f, false
+	);
+	
+}
+
+// Called every frame
+void AStrategyCamera::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SpringArm->TargetArmLength = FMath::FInterpTo( SpringArm->TargetArmLength, TargetZoom, DeltaTime, ZoomInterpSpeed );
+
+	FVector ClampedLocation = GetActorLocation();
+	ClampedLocation.X = FMath::Clamp( ClampedLocation.X, -MapBounds.X, MapBounds.X );
+	ClampedLocation.Y = FMath::Clamp( ClampedLocation.Y, -MapBounds.Y, MapBounds.Y );
+	SetActorLocation( ClampedLocation );
+
+}
+
+// Called to bind functionality to input
+void AStrategyCamera::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis( "MoveForward", this, &AStrategyCamera::MoveForward );
+	PlayerInputComponent->BindAxis( "MoveRight", this, &AStrategyCamera::MoveRight );
+
+	PlayerInputComponent->BindAction( "ZoomIn", IE_Pressed, this, &AStrategyCamera::ZoomIn );
+	PlayerInputComponent->BindAction( "ZoomOut", IE_Pressed, this, &AStrategyCamera::ZoomOut );
+
+}
+
+void AStrategyCamera::MoveForward( float Value )
+{
+	AddMovementInput( GetActorForwardVector(), Value );
+}
+void AStrategyCamera::MoveRight( float Value )
+{
+	AddMovementInput( GetActorRightVector(), Value );
+}
+
+void AStrategyCamera::ZoomIn()
+{
+	TargetZoom = FMath::Clamp( TargetZoom - ZoomSpeed, MinZoom, MaxZoom );
+}
+void AStrategyCamera::ZoomOut()
+{
+	TargetZoom = FMath::Clamp( TargetZoom + ZoomSpeed, MinZoom, MaxZoom );
+}
---
+++ Lords_Frontiers/Source/Lords_Frontiers/Public/Camera/StrategyCamera.h
@@ -0,0 +1,90 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+
+#include "StrategyCamera.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UFloatingPawnMovement;
+class UInputComponent;
+
+UCLASS()
+class LORDS_FRONTIERS_API AStrategyCamera : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AStrategyCamera();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// components
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = "Camera|Components" )
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = "Camera|Components" )
+	TObjectPtr<UCameraComponent> Camera;
+
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = "Camera|Components" )
+	TObjectPtr<UFloatingPawnMovement> MovementComponent;
+
+	// wasd
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Camera|Settings|Movement" )
+	float MoveSpeed = 2000.0f;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Camera|Settings|Movement" )
+	float MoveAcceleration = 8000.0f;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Camera|Settings|Movement" )
+	float MoveDeceleration = 4000.0f;
+
+	// zoom
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Camera|Settings|Zoom" )
+	float ZoomSpeed = 500.0f;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Camera|Settings|Zoom" )
+	float MinZoom = 500.0f;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Camera|Settings|Zoom" )
+	float MaxZoom = 5000.0f;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Camera|Settings|Zoom" )
+	float ZoomInterpSpeed = 10.0f;
+
+	// izometric
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Camera|Settings|Visual" )
+	float CameraPitch = -50.0f;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Camera|Settings|Visual" )
+	float CameraYaw = 45.0f;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Camera|Settings|Visual" )
+	float CameraLagSpeed = 5.0f;
+
+	// map restrictions
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Camera|Settings|Bounds" )
+	FVector2D MapBounds = FVector2D( 10000.0f, 10000.0f );
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void ZoomIn();
+	void ZoomOut();
+
+	float TargetZoom;
+};
