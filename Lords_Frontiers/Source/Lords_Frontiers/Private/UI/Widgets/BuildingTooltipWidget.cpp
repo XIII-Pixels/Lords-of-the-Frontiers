43d3c42c6bfaf8a4554865622f574acc992b0da4
Gregory-Hub <novikov.gri@gmail.com>
2026-02-05 23:54:49 +0300

    Translate resource names + remove non-resource building income text #296

--- Lords_Frontiers/Source/Lords_Frontiers/Private/UI/Widgets/BuildingTooltipWidget.cpp
+++ Lords_Frontiers/Source/Lords_Frontiers/Private/UI/Widgets/BuildingTooltipWidget.cpp
@@ -14,26 +14,30 @@ void UBuildingTooltipWidget::UpdateTooltip( ABuilding* buildingCDO )
 
 	Text_BuildingName->SetText( FText::FromString( buildingCDO->GetNameBuild() ) );
 
-	Text_Cost->SetText(
-	    FText::Format( FText::FromString( TEXT( "Cost | {0}" ) ), FormatResourceText( buildingCDO->GetBuildingCost() ) )
-	);
+	Text_Cost->SetText( FText::Format(
+	    FText::FromString( TEXT( "Стоимость | {0}" ) ), FormatResourceText( buildingCDO->GetBuildingCost() )
+	) );
 
 	Text_Maintenance->SetText( FText::Format(
-	    FText::FromString( TEXT( "Rent | {0}" ) ), FormatResourceText( buildingCDO->GetMaintenanceCost() )
+	    FText::FromString( TEXT( "Содержание | {0}" ) ), FormatResourceText( buildingCDO->GetMaintenanceCost() )
 	) );
 
 	if ( AResourceBuilding* resB = Cast<AResourceBuilding>( buildingCDO ) )
 	{
 		Text_Production->SetText( FText::Format(
-			FText::FromString( TEXT( "Income | {0}" ) ), FormatResourceText( resB->GetProductionConfig() )
+		    FText::FromString( TEXT( "Доход | {0}" ) ), FormatResourceText( resB->GetProductionConfig() )
 		) );
+	}
+	else
+	{
+		Text_Production->SetText( FText::FromString( "" ) );
 	}
 }
 
 FText UBuildingTooltipWidget::FormatResourceText( const FResourceProduction& production )
 {
 	return FText::Format(
-	    FText::FromString( TEXT( "Gold: {0}, Food: {1}, Population: {2}, Progress: {3}" ) ), production.Gold,
+	    FText::FromString( TEXT( "Золото: {0}, Еда: {1}, Население: {2}, Прогресс: {3}" ) ), production.Gold,
 	    production.Food, production.Population, production.Progress
 	);
 }
