7f252f362da960c83c662dd85c0742d59007b6a1
Maxim Devkin <makintosh365@gmail.com>
2026-03-28 22:08:59 +0300

    Move construction VFX spawning from BuildManager to Building::BeginPlay #507

--- Lords_Frontiers/Source/Lords_Frontiers/Private/Building/Building.cpp
+++ Lords_Frontiers/Source/Lords_Frontiers/Private/Building/Building.cpp
@@ -56,6 +56,7 @@ void ABuilding::BeginPlay()
 	{
 	}
 	ResolveVFXDefaults();
+	SpawnConstructionVFX();
 }
 
 void ABuilding::OnDeath()
@@ -87,6 +88,8 @@ void ABuilding::ResolveVFXDefaults()
 	ResolvedHitVFX_ = HitVFX_;
 	ResolvedDestructionVFX_ = DestructionVFX_;
 	ResolvedRuinDelay_ = 0.0f;
+	ResolvedConstructionVFX_ = nullptr;
+	ResolvedConstructionDelay_ = 0.0f;
 
 	if ( UCoreManager* core = UCoreManager::Get( this ) )
 	{
@@ -108,6 +111,16 @@ void ABuilding::ResolveVFXDefaults()
 				{
 					ResolvedRuinDelay_ = classOverride->RuinDelay;
 				}
+
+				if ( classOverride->ConstructionVFX )
+				{
+					ResolvedConstructionVFX_ = classOverride->ConstructionVFX;
+				}
+
+				if ( classOverride->ConstructionDelay >= 0.0f )
+				{
+					ResolvedConstructionDelay_ = classOverride->ConstructionDelay;
+				}
 			}
 
 			if ( !ResolvedHitVFX_ )
@@ -124,7 +137,46 @@ void ABuilding::ResolveVFXDefaults()
 			{
 				ResolvedRuinDelay_ = config->DefaultRuinDelay;
 			}
+
+			if ( !ResolvedConstructionVFX_ )
+			{
+				ResolvedConstructionVFX_ = config->DefaultBuildingConstructionVFX;
+			}
+
+			if ( ResolvedConstructionDelay_ <= 0.0f )
+			{
+				ResolvedConstructionDelay_ = config->DefaultConstructionDelay;
+			}
 		}
+	}
+}
+
+
+void ABuilding::SpawnConstructionVFX()
+{
+	if ( !ResolvedConstructionVFX_ )
+	{
+		return;
+	}
+
+	if ( ResolvedConstructionDelay_ > 0.0f )
+	{
+		GetWorldTimerManager().SetTimer(
+		    ConstructionVFXTimerHandle_,
+		    [this]()
+		    {
+			    UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			        GetWorld(), ResolvedConstructionVFX_, GetActorLocation(), GetActorRotation()
+			    );
+		    },
+		    ResolvedConstructionDelay_, false
+		);
+	}
+	else
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+		    GetWorld(), ResolvedConstructionVFX_, GetActorLocation(), GetActorRotation()
+		);
 	}
 }
 
@@ -270,6 +322,7 @@ FVector ABuilding::GetSelectionLocation_Implementation() const
 void ABuilding::EndPlay( const EEndPlayReason::Type EndPlayReason )
 {
 	GetWorldTimerManager().ClearTimer( RuinTimerHandle_ );
+	GetWorldTimerManager().ClearTimer( ConstructionVFXTimerHandle_ );
 
 	if ( EconomyComponent_ )
 	{
--- Lords_Frontiers/Source/Lords_Frontiers/Public/Building/Building.h
+++ Lords_Frontiers/Source/Lords_Frontiers/Public/Building/Building.h
@@ -140,10 +140,18 @@ protected:
 	UPROPERTY()
 	TObjectPtr<UNiagaraSystem> ResolvedDestructionVFX_;
 
+	UPROPERTY()
+	TObjectPtr<UNiagaraSystem> ResolvedConstructionVFX_;
+
 	float ResolvedRuinDelay_ = 0.0f;
+	float ResolvedConstructionDelay_ = 0.0f;
+
+	void SpawnConstructionVFX();
 
 private:
 	FTimerHandle RuinTimerHandle_;
+	FTimerHandle ConstructionVFXTimerHandle_;
+
 
 	FResourceProduction OriginalMaintenanceCost_;
 	UPROPERTY( EditAnywhere, Category = "Settings|Visuals" )
