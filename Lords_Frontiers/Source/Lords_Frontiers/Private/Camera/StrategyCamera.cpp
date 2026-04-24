ecb2cc55c749d10a5f1acc96e4417ac1f156f8dc
Maxim Devkin <makintosh365@gmail.com>
2026-04-24 23:06:41 +0300

    Reapply "refactor camera #548 (#557)"
    This reverts commit f0fbb3d8c9c08866fec51bef87446d2472391b8a.

--- Lords_Frontiers/Source/Lords_Frontiers/Private/Camera/StrategyCamera.cpp
+++ Lords_Frontiers/Source/Lords_Frontiers/Private/Camera/StrategyCamera.cpp
@@ -1,28 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Camera/StrategyCamera.h"
+
 #include "Grid/GridManager.h"
 #include "UI/GameHUD.h"
 
-#include "GameFramework/SpringArmComponent.h"
-#include "Camera/CameraComponent.h"
-#include "GameFramework/PlayerController.h"
-#include "Components/InputComponent.h"
-#include "Kismet/GameplayStatics.h"
-#include "Camera/CameraActor.h"
 #include "Blueprint/WidgetBlueprintLibrary.h"
-
-#include "GameFramework/FloatingPawnMovement.h"
-
+#include "Camera/CameraActor.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AStrategyCamera::AStrategyCamera()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bTickEvenWhenPaused = true;
 
 	RootComponent = CreateDefaultSubobject<USceneComponent>( TEXT( "Root" ) );
 
@@ -42,18 +38,50 @@ AStrategyCamera::AStrategyCamera()
 
 	Camera->ProjectionMode = ECameraProjectionMode::Orthographic;
 	Camera->OrthoWidth = 2048.0f;
+	SpringArm->TargetArmLength = 2000.0f;
+
+	Camera->OrthoNearClipPlane = -2000.0f;
+	Camera->OrthoFarClipPlane = 10000.0f;
 
 	MovementComponent = CreateDefaultSubobject<UFloatingPawnMovement>( TEXT( "MovementComponent" ) );
 
 	TargetZoom_ = 2048.0f;
 	TargetYaw_ = CameraYaw_;
 	CurrentYaw_ = CameraYaw_;
+
+	if ( GetWorld() )
+	{
+		UKismetSystemLibrary::ExecuteConsoleCommand(
+		    GetWorld(), TEXT( "r.Shadow.Virtual.ResolutionLodBiasDirectional -2" )
+		);
+
+		UKismetSystemLibrary::ExecuteConsoleCommand( GetWorld(), TEXT( "r.Shadow.Virtual.ClipMargin 0.25" ) );
+
+		UKismetSystemLibrary::ExecuteConsoleCommand(
+		    GetWorld(), TEXT( "r.Shadow.Virtual.SMRT.RayCountDirectional 4" )
+		);
+	}
 }
 
 // Called when the game starts or when spawned
 void AStrategyCamera::BeginPlay()
 {
 	Super::BeginPlay();
+
+	Camera->SetProjectionMode( ProjectionMode_ );
+
+	if ( ProjectionMode_ == ECameraProjectionMode::Orthographic )
+	{
+		Camera->OrthoWidth = InitialOrthoWidth_;
+		TargetZoom_ = InitialOrthoWidth_;
+		SpringArm->TargetArmLength = 3000.0f; //fix
+	}
+	else //Perspective
+	{
+		Camera->SetFieldOfView( FieldOfView_ );
+		SpringArm->TargetArmLength = InitialTargetArmLength_;
+		TargetZoom_ = InitialTargetArmLength_;
+	}
 
 	if ( APlayerController* pc = Cast<APlayerController>( GetController() ) )
 	{
@@ -79,7 +107,7 @@ void AStrategyCamera::BeginPlay()
 	MovementComponent->Acceleration = MoveAcceleration_;
 	MovementComponent->Deceleration = MoveDeceleration_;
 
-	SpringArm->TargetArmLength = 2000.0f;
+	SpringArm->TargetArmLength = 3000.0f;
 	SpringArm->CameraLagSpeed = CameraLagSpeed_;
 	SpringArm->SetRelativeRotation( FRotator( CameraPitch_, CameraYaw_, 0.0f ) );
 
@@ -103,11 +131,6 @@ void AStrategyCamera::BeginPlay()
 
 			MinMapBounds_ = FVector2D( gridOrigin.X, gridOrigin.Y );
 			MaxMapBounds_ = FVector2D( gridOrigin.X + gridWidth, gridOrigin.Y + gridHeight );
-
-			UE_LOG(
-			    LogTemp, Warning, TEXT( "Camera: Bounds set from Grid! Min: %s, Max: %s" ), *MinMapBounds_.ToString(),
-			    *MaxMapBounds_.ToString()
-			);
 		}
 	}
 
@@ -125,21 +148,35 @@ void AStrategyCamera::BeginPlay()
 	    },
 	    0.2f, false
 	);
-	
 }
 
 // Called every frame
-void AStrategyCamera::Tick(float deltaTime)
+void AStrategyCamera::Tick( float deltaTime )
 {
-	Super::Tick(deltaTime);
+	Super::Tick( deltaTime );
 
-	Camera->OrthoWidth = FMath::FInterpTo( Camera->OrthoWidth, TargetZoom_, deltaTime, ZoomInterpSpeed_ );
+	float GlobalTimeDilation = UGameplayStatics::GetGlobalTimeDilation( GetWorld() );
+	float TimeScale = ( GlobalTimeDilation > 0.0f ) ? ( 1.0f / GlobalTimeDilation ) : 1.0f;
+
+	float RealDeltaTime = deltaTime * TimeScale;
+
+	MovementComponent->MaxSpeed = MoveSpeed_ * TimeScale;
+	MovementComponent->Acceleration = MoveAcceleration_ * TimeScale;
+	MovementComponent->Deceleration = MoveDeceleration_ * TimeScale;
+
+	if ( ProjectionMode_ == ECameraProjectionMode::Orthographic )
+	{
+		Camera->OrthoWidth = FMath::FInterpTo( Camera->OrthoWidth, TargetZoom_, RealDeltaTime, ZoomInterpSpeed_ );
+	}
+	else
+	{
+		SpringArm->TargetArmLength =
+		    FMath::FInterpTo( SpringArm->TargetArmLength, TargetZoom_, RealDeltaTime, ZoomInterpSpeed_ );
+	}
 
 	FRotator currentRot = SpringArm->GetRelativeRotation();
-
 	FRotator targetRot = FRotator( CameraPitch_, TargetYaw_, 0.0f );
-
-	FRotator newRot = FMath::RInterpTo( currentRot, targetRot, deltaTime, RotationSpeed_ * 0.1f );
+	FRotator newRot = FMath::RInterpTo( currentRot, targetRot, RealDeltaTime, RotationSpeed_ * 0.1f );
 	SpringArm->SetRelativeRotation( newRot );
 
 	if ( bEnableEdgeScrolling_ )
@@ -157,7 +194,7 @@ void AStrategyCamera::Tick(float deltaTime)
 }
 
 // Called to bind functionality to input
-void AStrategyCamera::SetupPlayerInputComponent(UInputComponent* playerInputComponent)
+void AStrategyCamera::SetupPlayerInputComponent( UInputComponent* playerInputComponent )
 {
 	Super::SetupPlayerInputComponent( playerInputComponent );
 
@@ -179,11 +216,8 @@ void AStrategyCamera::SetupPlayerInputComponent(UInputComponent* playerInputComp
 
 void AStrategyCamera::Move( const FInputActionValue& value )
 {
-
 	if ( bIsCameraInputDisabled_ )
-	{
 		return;
-	}
 
 	FVector2D movementVector = value.Get<FVector2D>();
 
@@ -200,44 +234,30 @@ void AStrategyCamera::Move( const FInputActionValue& value )
 void AStrategyCamera::Zoom( const FInputActionValue& value )
 {
 	if ( bIsCameraInputDisabled_ )
-	{
 		return;
-	}
-	
-	float zoomDirection = value.Get<float>();
 
+	float zoomDirection = value.Get<float>();
 	TargetZoom_ = FMath::Clamp( TargetZoom_ - ( zoomDirection * ZoomSpeed_ ), MinZoom_, MaxZoom_ );
 }
 
 void AStrategyCamera::Rotate( const FInputActionValue& value )
 {
 	if ( bIsCameraInputDisabled_ )
-	{
 		return;
-	}
 
-	//Value = 1 (E) -1 (Q)
 	float direction = value.Get<float>();
 
 	if ( direction != 0.0f )
 	{
 		TargetYaw_ += direction * 90.0f;
-
 		TargetYaw_ = FRotator::NormalizeAxis( TargetYaw_ );
-
-		if ( GEngine )
-			GEngine->AddOnScreenDebugMessage(
-			    -1, 2.f, FColor::Cyan, FString::Printf( TEXT( "New Target Yaw: %f" ), TargetYaw_ )
-			);
 	}
 }
 
 void AStrategyCamera::HandleEdgeScrolling()
 {
 	if ( bIsCameraInputDisabled_ )
-	{
 		return;
-	}
 
 	if ( APlayerController* PC = Cast<APlayerController>( GetController() ) )
 	{
@@ -250,21 +270,15 @@ void AStrategyCamera::HandleEdgeScrolling()
 			FVector2D movementInput( 0.f, 0.f );
 
 			if ( mouseX <= EdgeScrollThreshold_ )
-			{
 				movementInput.X = -1.f;
-			}	
 			else if ( mouseX >= viewportX - EdgeScrollThreshold_ )
-			{
 				movementInput.X = 1.f;
-			}
+
 			if ( mouseY <= EdgeScrollThreshold_ )
-			{
 				movementInput.Y = 1.f;
-			}
 			else if ( mouseY >= viewportY - EdgeScrollThreshold_ )
-			{
 				movementInput.Y = -1.f;
-			}
+
 			if ( !movementInput.IsZero() )
 			{
 				const float cCurrentYaw = SpringArm->GetComponentRotation().Yaw;
