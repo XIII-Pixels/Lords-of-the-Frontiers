33139f5737dd8e1b880869383fafae32904bbc22
TToJlkoBHuK <28arhalex@gmail.com>
2026-04-27 00:01:45 +0300

    update content cloud #587

--- Lords_Frontiers/Source/Lords_Frontiers/Private/Camera/CloudBorderManager.cpp
+++ Lords_Frontiers/Source/Lords_Frontiers/Private/Camera/CloudBorderManager.cpp
@@ -43,20 +43,22 @@ void ACloudBorderManager::GenerateClouds()
 	if ( CloudISMCs.Num() == 0 || CloudSpacing <= 0.0f )
 		return;
 
-	FVector Center = GetActorLocation();
+	FVector Center = FVector::ZeroVector;
+
+	FRandomStream RNG( 12345 );
 
 	auto SpawnCloud = [&]( FVector Position, FVector OutwardDir )
 	{
-		Position.X += FMath::RandRange( -RandomOffsetRange, RandomOffsetRange );
-		Position.Y += FMath::RandRange( -RandomOffsetRange, RandomOffsetRange );
+		Position.X += RNG.FRandRange( -RandomOffsetRange, RandomOffsetRange );
+		Position.Y += RNG.FRandRange( -RandomOffsetRange, RandomOffsetRange );
 
-		float Scale = BaseCloudScale * FMath::RandRange( 0.8f, 1.5f );
+		float Scale = BaseCloudScale * RNG.FRandRange( 0.8f, 1.5f );
 		FRotator RandRot = CloudFacingRotation;
-		RandRot.Roll += FMath::RandRange( -15.0f, 15.0f );
+		RandRot.Roll += RNG.FRandRange( -15.0f, 15.0f );
 
 		FTransform Transform( RandRot, Position, FVector( Scale ) );
 
-		int32 MatIndex = FMath::RandRange( 0, CloudISMCs.Num() - 1 );
+		int32 MatIndex = RNG.RandRange( 0, CloudISMCs.Num() - 1 );
 		int32 InstIndex = CloudISMCs[MatIndex]->AddInstance( Transform );
 
 		FCloudInstanceData Data;
@@ -67,15 +69,28 @@ void ACloudBorderManager::GenerateClouds()
 		CloudsData.Add( Data );
 	};
 
-	for ( float x = -MapExtents.X; x <= MapExtents.X; x += CloudSpacing )
+	int32 NumLayers = 4;
+
+	for ( int32 Layer = 0; Layer < NumLayers; ++Layer )
 	{
-		SpawnCloud( Center + FVector( x, MapExtents.Y, 0 ), FVector( 0, 1, 0 ) );
-		SpawnCloud( Center + FVector( x, -MapExtents.Y, 0 ), FVector( 0, -1, 0 ) );
-	}
-	for ( float y = -MapExtents.Y; y <= MapExtents.Y; y += CloudSpacing )
-	{
-		SpawnCloud( Center + FVector( MapExtents.X, y, 0 ), FVector( 1, 0, 0 ) );
-		SpawnCloud( Center + FVector( -MapExtents.X, y, 0 ), FVector( -1, 0, 0 ) );
+		float CurrentRx = MapExtents.X + ( Layer * CloudSpacing * 0.8f );
+		float CurrentRy = MapExtents.Y + ( Layer * CloudSpacing * 0.8f );
+
+		float Perimeter = 2.0f * PI * FMath::Sqrt( ( CurrentRx * CurrentRx + CurrentRy * CurrentRy ) / 2.0f );
+		int32 CloudsInThisLayer = FMath::CeilToInt( Perimeter / CloudSpacing );
+
+		for ( int32 i = 0; i < CloudsInThisLayer; ++i )
+		{
+			float Angle = ( (float) i / (float) CloudsInThisLayer ) * 2.0f * PI;
+
+			float X = CurrentRx * FMath::Cos( Angle );
+			float Y = CurrentRy * FMath::Sin( Angle );
+
+			FVector SpawnPos = Center + FVector( X, Y, 0.0f );
+			FVector OutwardDir = FVector( X, Y, 0.0f ).GetSafeNormal();
+
+			SpawnCloud( SpawnPos, OutwardDir );
+		}
 	}
 }
 
@@ -104,6 +119,11 @@ void ACloudBorderManager::Tick( float DeltaTime )
 
 	CurrentZoomAlpha = FMath::FInterpTo( CurrentZoomAlpha, TargetAlpha, DeltaTime, TransitionSpeed );
 
+	FRotator CamRot = ActiveCamera->GetComponentRotation();
+	FRotator FaceCameraRot = CamRot;
+	FaceCameraRot.Pitch += 90.0f;
+	FQuat LocalRot = GetActorTransform().InverseTransformRotation( FaceCameraRot.Quaternion() );
+
 	for ( const FCloudInstanceData& Data : CloudsData )
 	{
 		float HideFactor = 1.0f - CurrentZoomAlpha;
@@ -114,11 +134,14 @@ void ACloudBorderManager::Tick( float DeltaTime )
 
 		FTransform NewTransform = Data.BaseTransform;
 		NewTransform.SetLocation( NewLocation );
-		NewTransform.SetScale3D(
-		    Data.BaseTransform.GetScale3D() * FMath::Max( 0.1f, CurrentZoomAlpha )
-		);
 
-		CloudISMCs[Data.ComponentIndex]->UpdateInstanceTransform( Data.InstanceIndex, NewTransform, true, false );
+		FRotator FinalRot = LocalRot.Rotator();
+		FinalRot.Roll += Data.BaseTransform.Rotator().Roll;
+		NewTransform.SetRotation( FinalRot.Quaternion() );
+
+		NewTransform.SetScale3D( Data.BaseTransform.GetScale3D() * FMath::Max( 0.1f, CurrentZoomAlpha ) );
+
+		CloudISMCs[Data.ComponentIndex]->UpdateInstanceTransform( Data.InstanceIndex, NewTransform, false, false );
 	}
 
 	for ( UInstancedStaticMeshComponent* ISMC : CloudISMCs )
