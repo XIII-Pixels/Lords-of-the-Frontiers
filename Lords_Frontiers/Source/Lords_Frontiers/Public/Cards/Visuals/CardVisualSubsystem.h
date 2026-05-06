796f9d8ef65fcdb29adef06c7e095cc89d372162
Makintosh365 <156786742+Makintosh365@users.noreply.github.com>
2026-05-06 16:56:13 +0300

    add card effect overlay and NS (burn and slow) #722 (#723)

--- Lords_Frontiers/Source/Lords_Frontiers/Public/Cards/Visuals/CardVisualSubsystem.h
+++ Lords_Frontiers/Source/Lords_Frontiers/Public/Cards/Visuals/CardVisualSubsystem.h
@@ -9,8 +9,10 @@
 
 class ACardFeedbackPopup;
 class ACardIconStrip;
+class UMaterialInterface;
 class UNiagaraComponent;
 class UNiagaraSystem;
+class USkeletalMeshComponent;
 class UTexture2D;
 
 USTRUCT()
@@ -29,6 +31,15 @@ struct FCardStickyRecord
 
 	UPROPERTY()
 	TWeakObjectPtr<UNiagaraComponent> NiagaraComponent;
+
+	UPROPERTY()
+	TWeakObjectPtr<USkeletalMeshComponent> OverlayMesh;
+
+	UPROPERTY()
+	TWeakObjectPtr<UMaterialInterface> OverlayPrevMaterial;
+
+	UPROPERTY()
+	bool bOverlayApplied = false;
 
 	UPROPERTY()
 	int32 Id = INDEX_NONE;
@@ -99,9 +110,13 @@ private:
 
 	void PlayIcon( const FCardIconSpec& spec, AActor* owner, AActor* target, bool bSticky, int32 stickyId );
 	void PlayNiagara( const FCardNiagaraSpec& spec, AActor* owner, AActor* target, bool bSticky, int32 stickyId );
+	void PlayOverlay( const FCardOverlayMaterialSpec& spec, AActor* owner, AActor* target, int32 stickyId );
 
 	void SpawnIconNow( const FCardIconSpec& spec, AActor* host, bool bSticky, int32 stickyId );
 	void SpawnNiagaraNow( const FCardNiagaraSpec& spec, AActor* host, bool bSticky, int32 stickyId );
+	void ApplyOverlayNow( const FCardOverlayMaterialSpec& spec, AActor* host, int32 stickyId );
+
+	static USkeletalMeshComponent* FindSkeletalMeshOn( AActor* host );
 
 	void ResolveTargetHosts(
 		ECardVisualTarget target, AActor* owner, AActor* hitTarget, TArray<AActor*>& outHosts ) const;
